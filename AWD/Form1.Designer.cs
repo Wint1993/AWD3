2da5c007f639a1b03ebb8702dd9e5b08d821e441
Błażej Rejnowski <rejnowski@gmail.com>
2018-01-20 12:51:59 +0100

    Dodaj pliki projektów.

---
+++ AWD/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿using System.Threading;
+
+namespace AWD
+{
+    partial class Form1
+    {
+      
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chart = new AForge.Controls.Chart();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 455);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(16, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(201, 395);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Loaded data";
+            this.columnHeader1.Width = 87;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Estimated Data";
+            this.columnHeader2.Width = 93;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chart);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(269, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(502, 455);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Prediction";
+            // 
+            // chart
+            // 
+            this.chart.Location = new System.Drawing.Point(9, 19);
+            this.chart.Name = "chart";
+            this.chart.RangeX = ((AForge.Range)(resources.GetObject("chart.RangeX")));
+            this.chart.RangeY = ((AForge.Range)(resources.GetObject("chart.RangeY")));
+            this.chart.Size = new System.Drawing.Size(493, 283);
+            this.chart.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(304, 379);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(88, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(304, 350);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(88, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(304, 322);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(88, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(115, 379);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(83, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 350);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(83, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 322);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(226, 382);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Prediction error";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(226, 353);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Learning error";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(226, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Iteration";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Omitted end points";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Omitted initial points";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Learning Ratio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 477);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+  
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private AForge.Controls.Chart chart;
+    }
+}
+
---
+++ AWD/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+using AForge.Controls;
+using System.Threading;
+using System.IO;
+
+namespace AWD
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            graphSettings();
+            updateSettings();
+        }
+
+        private void graphSettings()
+        {
+            chart.AddDataSeries("data", Color.Red, Chart.SeriesType.Dots, 5);
+            chart.AddDataSeries("solution", Color.Blue, Chart.SeriesType.Line, 1);
+            chart.AddDataSeries("omittedInitialPoints", Color.LightGray, Chart.SeriesType.Line, 1, false);
+            chart.AddDataSeries("omittedEndPoints", Color.Gray, Chart.SeriesType.Line, 1, false);
+        }
+
+        ActivationNetwork network;
+        BackPropagationLearning teacher;
+
+        private double learnRatio = 0.1;
+        private double[] inputData = null;
+        private double[,] displayData = null;
+        private double[,] initialDelimiter = new double[2, 2] { { 0, 0 }, { 0, 0 } };
+        private double[,] closingDelimiter = new double[2, 2] { { 0, 0 }, { 0, 0 } };
+
+        private int MAXOMITTEDENDPOINTS = 14;
+        private int MINOMITTEDINITIALPOINTS = 15;
+
+        private int amountOfOmittedInitialPoints = 1;
+        private int amountOfOmittedEndsPoints = 1;
+        private int iterations = 10000;
+        private int dataQuantity = 252;
+
+        private Thread thread = null;
+        private volatile bool needToStop = false;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader rd = null;
+               
+                // Wczytywanie danych
+                double[] tmp = new double[dataQuantity];
+
+                try
+                {
+                    rd = File.OpenText(openFileDialog1.FileName);
+                    string str = null;
+                    int i = 0;
+
+                    // Wczytywanie danych
+                    while ((i < dataQuantity) && ((str = rd.ReadLine()) != null))
+                    {
+                        // Parsowanie danych
+                        tmp[i] = double.Parse(str);
+
+                        i++;
+                    }
+
+                    // Ustawianie danych
+                    inputData = new double[i];
+                    displayData = new double[i, 2];
+                    Array.Copy(tmp, 0, inputData, 0, i);
+                    for (int j = 0; j < i; j++)
+                    {
+                        displayData[j, 0] = j;
+                        displayData[j, 1] = inputData[j];
+                    }
+                }
+                finally
+                {
+                    if (rd != null)
+                        rd.Close();
+                }
+
+                // Aktualizacjia kolumn danych oraz wykresu
+                updateDataList();
+                chart.RangeX = new Range(0, inputData.Length - 1);
+                chart.UpdateDataSeries("data", displayData);
+                chart.UpdateDataSeries("solution", null);
+                // Ustawienie ograniczen 
+                setDelimiters();
+             
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Czyszczenie oraz ustawienie początkowych danych
+            clearSolution();
+            try
+            {
+                learnRatio = Math.Max(0.00001, Math.Min(1, double.Parse(textBox1.Text)));
+            }
+            catch
+            {
+                learnRatio = 0.1;
+            }  
+         
+            updateSettings();
+            needToStop = false;
+            thread = new Thread(new ThreadStart(findSolution));
+            thread.Start();
+        }
+
+        private void clearSolution()
+        {
+            for (int j = 0, n = inputData.Length; j < n; j++)
+            {
+                if (listView1.Items[j].SubItems.Count > 1)
+                    listView1.Items[j].SubItems.RemoveAt(1);
+            }
+        }
+
+        private delegate void SetTextCallback(System.Windows.Forms.Control control, string text);
+        private delegate void AddSubItemCallback(System.Windows.Forms.ListView control, int item, string subitemText);
+
+        // Funkcja służy do aktualizowania danych zmieniających sie w cz
+        private void updateText(System.Windows.Forms.Control c, string txt)
+        {
+            if (c.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(updateText);
+                Invoke(d, new object[] { c, txt });
+            }
+            else
+            {
+                c.Text = txt;
+            }
+        }
+
+        private void addSolutionToEstimatedColumn(System.Windows.Forms.ListView control, int item, string itemTxt)
+        {
+            if (control.InvokeRequired)
+            {
+                AddSubItemCallback d = new AddSubItemCallback(addSolutionToEstimatedColumn);
+                Invoke(d, new object[] { control, item, itemTxt });
+            }
+            else
+            {
+                control.Items[item].SubItems.Add(itemTxt);
+            }
+        }
+
+        // Aktualizowanie parametrów wejściowych
+        private void updateSettings()
+        {
+            textBox1.Text = learnRatio.ToString();
+            textBox3.Text = amountOfOmittedInitialPoints.ToString();
+            textBox4.Text = amountOfOmittedEndsPoints.ToString();
+        }
+
+        // Ustawianie lini progowych (ograniczających pole działa sieci neuronowej) 
+        private void setDelimiters()
+        {
+            initialDelimiter[0, 0] = initialDelimiter[1, 0] = amountOfOmittedInitialPoints;
+            initialDelimiter[0, 1] = chart.RangeY.Min;
+            initialDelimiter[1, 1] = chart.RangeY.Max;
+            chart.UpdateDataSeries("omittedInitialPoints", initialDelimiter);
+            closingDelimiter[0, 0] = closingDelimiter[1, 0] = inputData.Length - 1 - amountOfOmittedEndsPoints;
+            closingDelimiter[0, 1] = chart.RangeY.Min;
+            closingDelimiter[1, 1] = chart.RangeY.Max;
+            chart.UpdateDataSeries("omittedEndPoints", closingDelimiter);
+        }
+
+        // Aktualizacja danych wejsciowych
+        private void updateDataList()
+        {
+            listView1.Items.Clear();
+            for (int i = 0, n = inputData.GetLength(0); i < n; i++)
+            {
+                listView1.Items.Add(inputData[i].ToString());
+            }
+        }
+
+        // Ustawianie lini progowej (ograniczającej) początkowej
+        private void updateOmittedInitialPointsQuantity()
+        {
+            if (inputData != null)
+            {
+                // Ustawienie nowej  lini progowej
+                try
+                {
+                    amountOfOmittedInitialPoints = Math.Max(1, Math.Min(MINOMITTEDINITIALPOINTS, int.Parse(textBox3.Text)));
+                }
+                catch
+                {
+                    amountOfOmittedInitialPoints = 1;
+                }
+                // Warunek, który musi być spełniony - jeżeli wyjedziemy linią progową za obszar końcowy danych
+                if (amountOfOmittedInitialPoints >= inputData.Length)
+                   amountOfOmittedInitialPoints = 1;
+                // Aktualizacja ograniczników
+                setDelimiters();
+            }
+        }
+
+        // Ustawianie lini progowej (ograniczającej) końcowej
+        private void updateOmittedEndsPointsQuantity()
+        {
+            if (inputData != null)
+            {
+                // Ustawienie nowej  lini progowej
+                try
+                {
+                    amountOfOmittedEndsPoints = Math.Max(1, Math.Min(MAXOMITTEDENDPOINTS, int.Parse(textBox4.Text)));
+                }
+                catch
+                {
+                    amountOfOmittedEndsPoints = 1;
+                }
+                // Warunek, który musi być spełniony - jeżeli wyjedziemy linią progową za obszar początkowy danych
+                if (inputData.Length - amountOfOmittedEndsPoints - 1 < amountOfOmittedInitialPoints)
+                    amountOfOmittedEndsPoints = 1;
+                // Aktualizacja ograniczników
+                setDelimiters();
+            }
+        }
+
+        void findSolution()
+        {
+            // Ilość danych zbioru uczącego
+            int quantitySamples = inputData.Length - amountOfOmittedEndsPoints - amountOfOmittedInitialPoints;
+            // Różnica pomiędzy wartością największą a najmniejszą ze zbioru uczącego
+            double diffrenceMinMaxValue = chart.RangeY.Length;
+            // Współczynnik transformacji danych - factor - wartość oszacowania odchylenia standardowego populacji dla całkowitej zmienności
+            // Jest on użyty po to, żeby przy normalizacji danych nie przekroczyć zakresu -1 do 1
+            double dtc = 1.7 / diffrenceMinMaxValue;
+            //Najmniejsza wartość zbioru uczącego
+            double yMin = chart.RangeY.Min;
+            // Przygotowanie danych uczących
+            double[][] inputTmpData = new double[quantitySamples][];
+            double[][] outputTmpData = new double[quantitySamples][];
+
+            // Normalizacja danych z zakresu -1 do 1
+            for (int i = 0; i < quantitySamples; i++)
+            {
+                // Ustawienie nowych danych, nie włączając w to danych pominiętych przez linie ograniczające,
+                // przypisanie danych do tablicy outputTmpData
+                inputTmpData[i] = new double[amountOfOmittedInitialPoints];
+                outputTmpData[i] = new double[1];
+
+                //  Ustawienie danych wejściowych - normalizacja danych
+                for (int j = 0; j < amountOfOmittedInitialPoints; j++)
+                {
+                    inputTmpData[i][j] = (inputData[i + j] - yMin) * dtc - 0.85;
+                }
+                // Ustawienie danych końcowych
+                outputTmpData[i][0] = (inputData[i + amountOfOmittedInitialPoints] - yMin) * dtc - 0.85;
+            }
+
+            createMultiLayerNeuralNetwork(out network, out teacher);
+            int iteration = 1;
+
+            // Tablice rozwiązań
+            int solutionSize = inputData.Length - amountOfOmittedInitialPoints;
+            double[,] solution = new double[solutionSize, 2];
+            double[] networkInput = new double[amountOfOmittedInitialPoints];
+
+            // Obliczanie wartosci, które będą użyte do funkcji rozwiązującej - do przypisania danych dla tablicy estymowanej
+            for (int j = 0; j < solutionSize; j++)
+            {
+                solution[j, 0] = j + amountOfOmittedInitialPoints;
+            }
+
+            // Pętla rozpoczynająca uczenie 
+            while (!needToStop)
+            {
+                // Uruchamiamy epokę procedury uczenia
+                double error = teacher.RunEpoch(inputTmpData, outputTmpData) / quantitySamples;
+               
+                // Obliczanie wyniku oraz błędu uczenia i błędu predykcji
+                double learningError = 0.0;
+                double predictionError = 0.0;
+              
+                for (int i = 0, n = inputData.Length - amountOfOmittedInitialPoints; i < n; i++)
+                {
+                    // Wstawianie wartości bieżących(dla wartości od lini progowej poczatkowej) podlegających normalizacji
+                    for (int j = 0; j < amountOfOmittedInitialPoints; j++)
+                    {
+                        networkInput[j] = (inputData[i + j] - yMin) * dtc - 0.85;
+                    }
+
+                    // Ocenianie funkcji
+                    solution[i, 1] = (network.Compute(networkInput)[0] + 0.85) / dtc + yMin;
+
+                    //Obliczanie błędu predykcji
+                    if (i >= n - amountOfOmittedEndsPoints)
+                    {
+                        predictionError += Math.Abs(solution[i, 1] - inputData[amountOfOmittedInitialPoints + i]);
+                    }
+                    else
+                    {
+                        learningError += Math.Abs(solution[i, 1] - inputData[amountOfOmittedInitialPoints + i]);
+                    }
+                }
+                // Aktualizowanie wykresu
+                chart.UpdateDataSeries("solution", solution);
+
+                // Aktualizowanie danych - iteracji, błędu uczenia oraz blędu predykcji w czasie iteracji od 1 do 1000
+                updateText(textBox5, iteration.ToString());
+                updateText(textBox6, learningError.ToString("F3"));
+                updateText(textBox7, predictionError.ToString("F3"));
+                iteration++;
+
+                // Sprawdzenie czy trzeba się zatrzymać
+                if ((iterations != 0) && (iteration > iterations))
+                    break;
+            }
+
+            // Dodanie wyników do estymowanej kolumny wynikowej
+            for (int j = amountOfOmittedInitialPoints, k = 0, n = inputData.Length; j < n; j++, k++)
+            {
+                addSolutionToEstimatedColumn(listView1, j, solution[k, 1].ToString());
+            }
+
+        }
+
+
+        // Tworzenie sieci neuronowej
+        private void createMultiLayerNeuralNetwork(out ActivationNetwork network, out BackPropagationLearning teacher)
+        {
+            network = new ActivationNetwork(
+                new BipolarSigmoidFunction(),
+                amountOfOmittedInitialPoints, (amountOfOmittedInitialPoints * 2)+1, 1);
+            teacher = new BackPropagationLearning(network);
+            teacher.LearningRate = learnRatio;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            updateOmittedInitialPointsQuantity();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            updateOmittedEndsPointsQuantity();
+        }
+    }
+}
